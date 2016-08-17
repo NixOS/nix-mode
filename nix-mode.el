616c36889855d83415cb6fd72ad56606e84e40d6
Matthew Justin Bauer <mjbauer95@gmail.com>
2016-08-17 16:12:04 -0500

    Add two spaces after period.

--- nix-mode.el
+++ nix-mode.el
@@ -10,7 +10,7 @@
 
 ;;; Commentary:
 
-;; A major mode for editing Nix expressions (.nix files). See the Nix manual
+;; A major mode for editing Nix expressions (.nix files).  See the Nix manual
 ;; for more information available at https://nixos.org/nix/manual/.
 
 ;;; Code:
