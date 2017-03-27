1e58da56ac3830cd07deaf81b151817785ad3a23
Leon Isenberg <ljli@users.noreply.github.com>
2017-03-27 23:56:47 +0200

    Fix var name nix-err-msg-re

--- nix-mode.el
+++ nix-mode.el
@@ -186,7 +186,7 @@ If a close brace `}' ends an antiquote, the next character begins a string."
     (insert output)
     (goto-char (point-min))
     (let ((errs '()))
-      (while (search-forward-regexp err-msg-re nil t 1)
+      (while (search-forward-regexp nix-err-msg-re nil t 1)
         (let* ((file (match-string 2))
                (line (string-to-number (match-string 3)))
                (col (string-to-number (match-string 4)))
