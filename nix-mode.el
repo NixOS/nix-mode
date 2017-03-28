b1078c6adddb719f19b5ea258d8a93d919ccf524
Matthew Bauer <mjbauer95@gmail.com>
2017-03-28 09:51:38 -0500

    Add case sensitive searching

--- nix-mode.el
+++ nix-mode.el
@@ -410,6 +410,9 @@ The hook `nix-mode-hook' is run when Nix mode is started.
   (setq-local paragraph-start "[ \t]*\\(#+[ \t]*\\)?$")
   (setq-local paragraph-separate paragraph-start)
 
+  ;; Case sensitive searching
+  (setq-local case-fold-search nil)
+
   (easy-menu-add nix-mode-menu nix-mode-map))
 
 ;;;###autoload
