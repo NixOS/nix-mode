14d1bbb9367239faa4a7ac41288bef36b788a337
Matthew Bauer <mjbauer95@gmail.com>
2018-07-30 20:56:58 -0400

    Delete dir-locals.el

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (eval nix-shell-with-string "let pkgs = import <nixpkgs> {};
-in pkgs.runCommand \"nix-mode-shell\" {
-  nativeBuildInputs = with pkgs; [ emacs git nix ];
-} \"\"")))
