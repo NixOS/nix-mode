79507ee193d1296535ed905a0da5439d2c06aa43
Jakub Piecuch <j.piecuch96@gmail.com>
2019-06-15 14:13:28 +0200

    Add more test cases to smie-lambdas.nix

--- tests/testcases/smie-lambdas.nix
+++ tests/testcases/smie-lambdas.nix
@@ -2,7 +2,9 @@
 { foo, bar } @ baz:
 foo: bar:
 
-{
+{ test0 = x:
+    42;
+
   test1 =
     { f = x:
         42 };
@@ -17,10 +19,23 @@ foo: bar:
     { f = x: y: z:
         42 };
 
-  test2 = x: y: z:
+  test5 = x: y: z:
     42;
 
-  test3 = map (x: y: z:
+  test6 = map (x: y: z:
     42) [ 1 2 3 ];
-}
 
+  test7 = map
+    (x:
+      42);
+
+  test7 = map
+    (x: y: z:
+      42);
+
+  test8 =
+    x:
+    y:
+    z:
+    42;
+}
