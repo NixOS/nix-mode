1b1b9fb61d6ce4819d400ecefc7421be7623b369
Matthew Bauer <mjbauer95@gmail.com>
2021-06-17 23:20:38 -0500

    Update nixos

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "An emacs major mode for editing Nix expressions";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-20.09-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05-small";
 
   outputs = { self, nixpkgs }: let
-    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
   in {
     packages = forAllSystems (system: with (import nixpkgs { inherit system; }); {
